cc2404a0f896400d1f0972a91f7a7fad72f2b295
杨晓晨 <yangxiaochen@cyyz-health.com>
2016-07-19 11:21:44 +0800

    111111

--- app/src/main/java/com/example/cyyz/myjenkinstest/MainActivity.java
+++ app/src/main/java/com/example/cyyz/myjenkinstest/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.cyyz.myjenkinstest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @InjectView(R.id.tv_holleworldd)
+    TextView tvHolleworldd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        tvHolleworldd.setText("你好世界");
+    }
+
+    @OnClick(R.id.tv_holleworldd)
+    public void onClick() {
+        tvHolleworldd.setText("哈哈哈哈");
     }
 }
